e9bc16976de2e6f9576aa987829ba5cf7ea12462
Artem2003OnToday <bo4arovartyom@yandex.ru>
2022-05-01 21:59:49 +0300

    add WHAT???

---
+++ src/client.cpp
@@ -0,0 +1,44 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Channel;
+using grpc::ChannelArguments;
+using grpc::ClientContext;
+using grpc::Status;
+
+int main(int argc, char **argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  ChannelArguments args;
+  // Set the default compression algorithm for the channel.
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "include/game/game.h"
 
-int main() {
+void test_SFML() {
   game runned_game;
   while (runned_game.isRunning()) {
     runned_game.update();
     runned_game.render();
   }
+}
+
+void test_gRPC() {
+  
+}
+
+int main() {
+  test_SFML();
+  test_gRPC();
   return 0;
 }
