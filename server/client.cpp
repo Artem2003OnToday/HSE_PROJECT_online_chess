b3183ea17852c3c86095792e9fd6cbf1b511666d
Maestro code <jesus.me999@yandex.ru>
2022-05-17 18:32:03 +0300

    update server: we have Auth, StartGame and SearchGame; server without database

--- server/client.cpp
+++ server/client.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <mutex>
@@ -16,58 +17,73 @@ using grpc::ClientContext;
 using grpc::ClientReaderWriter;
 using grpc::Status;
 
-StartMessage MakeStartMessage(const std::string &name) {
-  StartMessage m;
-  m.set_name(name);
-  return m;
-}
+using namespace std::chrono;
+std::string token;
 
-class RouteGuideClient {
+class GameClient {
 public:
-  RouteGuideClient(std::shared_ptr<Channel> channel)
-      : stub_(RouteGuide::NewStub(channel)) {}
-  void GetIn() {
+  GameClient(std::shared_ptr<Channel> channel)
+      : stub_(Game::NewStub(channel)) {}
+  std::string Auth(const std::string &login, const std::string &password) {
+    AuthRequest request;
+    request.set_login(login);
+    request.set_password(password);
+    // request.set_ip("11.1.1.1");
+    request.set_time(std::to_string(
+        duration_cast<milliseconds>(system_clock::now().time_since_epoch())
+            .count()));
+
     ClientContext context;
-    std::shared_ptr<ClientReaderWriter<StartMessage, StartInfo>> stream(
-        stub_->GetIn(&context));
-
-    std::thread writer([stream]() {
-      std::vector<StartMessage> messages{MakeStartMessage("Artemka")};
-      for (const StartMessage &start : messages) {
-        std::cout << "Sending message " << start.name() << std::endl;
-        stream->Write(start);
-      }
-      stream->WritesDone();
-    });
-    StartInfo server_info;
-    while (stream->Read(&server_info)) {
-      std::unique_lock<std::mutex> lock(mu_);
-      std::cout << "Got message " << server_info.id() << ", "
-                << server_info.yourmotion() << std::endl;
+    AuthResponse response;
+    Status status = stub_->Auth(&context, request, &response);
+    if (status.ok()) {
+      return response.token();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
     }
+  }
+  bool StartGame() {
+    StartGameRequest request;
+    request.set_token(token);
 
-    std::cerr << "why not??" << std::endl;
-    // [&]() {
-    //   std::unique_lock<std::mutex> lock(mu_);
-    //   std::cerr << "send to server <<Artemka>>" << std::endl;
-    //   stream->Write(MakeStartMessage("Artemka"));
-    //   stream->WritesDone();
-    // }();
-    writer.join();
-    Status status = stream->Finish();
+    ClientContext context;
+    StartGameResponse response;
+    Status status = stub_->StartGame(&context, request, &response);
     if (!status.ok()) {
-      std::cout << "GetIn rpc failed." << std::endl;
+      std::cout << "StartGame fail...: " << status.error_code() << ": "
+                << status.error_message() << std::endl;
     }
+    return response.right_insert();
+  }
+  bool SearchGame() {
+    SearchGameRequest request;
+    request.set_token(token);
+
+    ClientContext context;
+    SearchGameResponse response;
+    Status status = stub_->SearchGame(&context, request, &response);
+    if (!status.ok()) {
+      std::cout << "StartGame fail...: " << status.error_code() << ": "
+                << status.error_message() << std::endl;
+    }
+    return response.search_accept();
   }
 
 private:
-  std::unique_ptr<RouteGuide::Stub> stub_;
+  std::unique_ptr<Game::Stub> stub_;
   std::mutex mu_;
 };
 
 int main(int argc, char **argv) {
-  RouteGuideClient guide(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
-  guide.GetIn();
+  GameClient guide(grpc::CreateChannel("localhost:50051",
+                                       grpc::InsecureChannelCredentials()));
+  token = guide.Auth("login", "password");
+  std::cout << token << std::endl;
+  guide.StartGame();
+  while (!guide.SearchGame()) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  }
   return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -17,48 +17,72 @@ using grpc::ServerContext;
 using grpc::ServerReaderWriter;
 using grpc::Status;
 
-StartInfo MakeStartInfo(int id, bool yourMotion) {
-  StartInfo i;
-  i.set_id(id);
-  i.set_yourmotion(yourMotion);
-  return i;
+enum BOARD_COLOR { WHITE, BLACK };
+
+std::string get_token(const std::string &ip, const std::string &time) {
+  return ip + ":" + time;
 }
 
-class RouteGuideImpl final : public RouteGuide::Service {
+class GameImpl final : public Game::Service {
 public:
-  explicit RouteGuideImpl() {}
-  Status GetIn(ServerContext *context,
-               ServerReaderWriter<StartInfo, StartMessage> *stream) override {
-    std::cerr << "server sending..." << std::endl;
-    [&]() {
-      std::unique_lock<std::mutex> lock(mu_);
-      stream->Write(MakeStartInfo(1, false));
-    }();
+  explicit GameImpl() {}
+  Status Auth(ServerContext *context, const AuthRequest *request,
+              AuthResponse *response) override { // save login-password
+    // std::cerr << context->peer() << std::endl;
+    std::string generate_token = get_token(context->peer(), request->time());
+    response->set_token(generate_token);
+    users_tokens.insert(generate_token);
+    return Status::OK;
+  }
+  Status StartGame(ServerContext *context, const StartGameRequest *request,
+                   StartGameResponse *response) override {
+    std::string user_token = request->token();
+    ready_to_play.insert(user_token);
+    response->set_right_insert(true);
+    return Status::OK;
+  }
+  Status SearchGame(ServerContext *context, const SearchGameRequest *request,
+                    SearchGameResponse *response) override {
+    std::string user_token = request->token();
+    if (!ready_to_play.count(user_token)) {
+      response->set_color(WHITE);
+      response->set_search_accept(true);
+    } else {
+      if (static_cast<int>(ready_to_play.size()) > 1) {
+        std::string first = user_token;
+        ready_to_play.erase(first);
+        std::string second = *ready_to_play.begin();
+        ready_to_play.erase(second);
 
-    std::cerr << "server read" << std::endl;
-    StartMessage note;
-    while (stream->Read(&note)) {
-      std::unique_lock<std::mutex> lock(mu_);
-      // stream->Write(MakeStartInfo(1, false));
-      std::cerr << note.name() << std::endl;
+        current_game[second] = first;
+        response->set_color(BLACK);
+        response->set_search_accept(true);
+      } else {
+        response->set_color(BLACK);
+        response->set_search_accept(false);
+      }
     }
-
     return Status::OK;
   }
 
 private:
   std::mutex mu_;
-  std::set<std::string> users;
+  std::set<std::string> users_tokens;
+  std::set<std::string> ready_to_play; // tokens
+  std::map<std::string, std::string> current_game;
 };
 
 void RunServer() {
   std::string server_address("0.0.0.0:50051");
-  RouteGuideImpl service;
+  GameImpl service;
   ServerBuilder builder;
+
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
+
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << "..." << std::endl;
+
   server->Wait();
 }
 
